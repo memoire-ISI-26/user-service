c741d86988bf21cfa1a4794de9a47710f76a1748
Ibrahim-svmb <ibrahim.svmb@gmail.com>
2026-07-13 16:26:14 +0000

    feat: set up circuit breaker

--- src/main/java/com/financedomain/user/proxy/TrackingProxy.java
+++ src/main/java/com/financedomain/user/proxy/TrackingProxy.java
@@ -1,13 +1,14 @@
 package com.financedomain.user.proxy;
 
 import com.financedomain.user.dto.TrackingEvent;
+import com.financedomain.user.proxy.fallback.TrackingProxyFallback;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 
-@FeignClient(name = "tracking-service")
+@FeignClient(name = "tracking-service", fallback = TrackingProxyFallback.class)
 public interface TrackingProxy {
 
     @PostMapping("/tracking/event")
--- src/main/java/com/financedomain/user/proxy/WalletProxy.java
+++ src/main/java/com/financedomain/user/proxy/WalletProxy.java
@@ -2,12 +2,13 @@ package com.financedomain.user.proxy;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.financedomain.user.dto.AccountCreationRequest;
+import com.financedomain.user.proxy.fallback.WalletProxyFallback;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "wallet-service")
+@FeignClient(name = "wallet-service", fallback = WalletProxyFallback.class)
 public interface WalletProxy {
 
     @PostMapping("/accounts")
