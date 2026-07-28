ac44dda79c0a3b830eeb820512e410d92e670350
Ibrahim-svmb <ibrahim.svmb@gmail.com>
2026-07-28 09:41:42 +0000

    feat: add specific exception

---
+++ src/main/java/com/financedomain/user/exception/NotAvailableException.java
@@ -0,0 +1,7 @@
+package com.financedomain.user.exception;
+
+public class NotAvailableException extends RuntimeException {
+    public NotAvailableException(String message) {
+        super(message);
+    }
+}
