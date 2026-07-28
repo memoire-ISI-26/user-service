b983dc8855db0513712b211c82d8e442a7acecd6
Ibrahim-svmb <ibrahim.svmb@gmail.com>
2026-07-28 09:42:30 +0000

    fix: use of specific exception

--- src/main/java/com/financedomain/user/proxy/fallback/WalletProxyFallback.java
+++ src/main/java/com/financedomain/user/proxy/fallback/WalletProxyFallback.java
@@ -1,6 +1,7 @@
 package com.financedomain.user.proxy.fallback;
 
 import com.financedomain.user.dto.AccountCreationRequest;
+import com.financedomain.user.exception.NotAvailableException;
 import com.financedomain.user.proxy.WalletProxy;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
@@ -12,6 +13,6 @@ public class WalletProxyFallback implements WalletProxy {
     public ResponseEntity<?> createAccount(AccountCreationRequest request) {
         System.err.println("[Fallback] wallet-service est indisponible. Impossible d'ouvrir un portefeuille pour le numéro : " + request.getNumber());
 
-        throw new RuntimeException("Le service financier (wallet-service) est actuellement indisponible. Veuillez réessayer plus tard.");
+        throw new NotAvailableException("Le service financier (wallet-service) est actuellement indisponible. Veuillez réessayer plus tard.");
     }
 }
